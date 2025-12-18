3a5ee1faeb65c49f4d1f62c6a7b49985364906fa
Aayush Borkar <aayushb14@gmail.com>
2025-12-18 22:11:08 +0530

    lualine location

--- nvim/init.lua
+++ nvim/init.lua
@@ -98,6 +98,10 @@ vim.cmd("colorscheme rose-pine")
 require("plugins.snacks")
 require("lualine").setup({
   sections = {
+    lualine_y = {
+      { "progress", separator = " ",                  padding = { left = 1, right = 0 } },
+      { "location", padding = { left = 0, right = 1 } },
+    },
     lualine_z = {
       function()
         return " " .. os.date("%R")
