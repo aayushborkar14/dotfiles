77ea0663f65249eb68dc2727ab2dde5338be33b5
Aayush Borkar <aayushb14@gmail.com>
2025-12-24 19:44:59 +0530

    Add vimtex

--- nvim/init.lua
+++ nvim/init.lua
@@ -88,7 +88,8 @@ vim.pack.add({
   "https://github.com/linux-cultist/venv-selector.nvim",
   "https://github.com/nvim-mini/mini.pairs",
   "https://github.com/nvim-mini/mini.surround",
-  "https://github.com/romainl/vim-cool"
+  "https://github.com/romainl/vim-cool",
+  "https://github.com/lervag/vimtex"
 })
 
 require("vim._extui").enable({}) -- https://github.com/neovim/neovim/pull/27855
