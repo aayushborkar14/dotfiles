1d7680b18376e82a0d09f4050bbb107be3ac1307
Aayush Borkar <aayushb14@gmail.com>
2026-01-07 15:07:11 +0530

    nvim autocmds

--- nvim/init.lua
+++ nvim/init.lua
@@ -28,6 +28,8 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.opt.clipboard:append("unnamedplus")
 
+-- Keymaps
+
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
@@ -78,6 +80,58 @@ keymap.set("n", "<C-w><down>", "C-w>-")
 keymap.set("n", "<C-j>", function()
   vim.diagnostic.goto_next()
 end, opts)
+
+-- Auto commands
+
+-- Resize splits when window is resized
+vim.api.nvim_create_autocmd("VimResized", {
+  pattern = "*",
+  callback = function()
+    local current_tab = vim.fn.tabpagenr()
+    vim.cmd("tabdo wincmd =")
+    vim.cmd("tabnext " .. current_tab)
+  end,
+})
+
+-- Go to last loc when opening a buffer
+vim.api.nvim_create_autocmd("BufReadPost", {
+  pattern = "*",
+  callback = function(event)
+    local exclude = { "gitcommit" }
+    local buf = event.buf
+    if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf].lazyvim_last_loc then
+      return
+    end
+    vim.b[buf].lazyvim_last_loc = true
+    local mark = vim.api.nvim_buf_get_mark(buf, '"')
+    local lcount = vim.api.nvim_buf_line_count(buf)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
+-- Fix conceal level for json files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "json", "jsonc", "json5" },
+  callback = function()
+    vim.opt_local.conceallevel = 0
+  end,
+})
+
+-- Auto create directories when saving a file
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function(event)
+    if event.match:match("^%w%w+:[\\/][\\/]") then
+      return
+    end
+    local file = vim.uv.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+  end,
+})
+
+-- Plugins
 
 vim.pack.add({
   { src = "https://github.com/rose-pine/neovim", name = "rose-pine" },
